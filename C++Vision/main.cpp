dcdb3cd3abd05230b489baea1356fdd08ae4e368
Cheryl <liaochery@yahoo.com>
2019-04-05 17:30:15 -0500

    Initial Commit

---
+++ C++Vision/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  C++Vision
+//
+//  Created by Cheryl Liao on 4/5/19.
+//  Copyright © 2019 Cheryl Liao. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
